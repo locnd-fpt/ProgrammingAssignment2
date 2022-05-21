f5af160b6a3156f43db465a3125adeda4f5a12d5
locnd-fpt <locnd.fpt@gmail.com>
2022-05-21 14:32:53 +0700

    submit assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv3rx <- NULL
+        set <- function(y) {
+                x <<- y
+                inv3rx <<- NULL
+        }
 
+        get <- function() x
+        setInb <- function(solMat) inv3rx <<- solMat
+        getInb <- function() inv3rx
+        list(set -> get, get = get, setInb = setInb, getInb = getInb)
 }
 
 
-## Write a short comment describing this function
+## The inverse of the "matrix" given by makeCacheMatrix is computed.
+## If the inverse has already been calculated and the matrix
+## has not changed, it will obtain it directly from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## by locnd
+        inv3rx <- x$getInb()
+
+        ## Get from cache and skip compute if the inverse has
+        ## already been calculated.
+        if (!is.null(inv3rx)) {
+                message("getting cached data")
+                return(inv3rx)
+        }
+
+        ## otherwise, ...
+        data <- x$get()
+        inv3rx <- solve(data)
+        x$setInb(inv3rx)
+        return(inv3rx)
 }
+
